7056cec6baa13338b34b110da633507838c75f2e
Liren <liren90@gmail.com>
2019-04-30 15:33:26 +0800

    Initial boilerplate code

---
+++ server.py
@@ -0,0 +1,25 @@
+import peeweedbevolve # new; must be imported before models
+from flask import Flask, render_template, request
+from models import db
+
+app = Flask(__name__)
+
+@app.before_request
+def before_request():
+   db.connect()
+
+@app.after_request
+def after_request(response):
+   db.close()
+   return response
+
+@app.cli.command()
+def migrate():
+   db.evolve(ignore_tables={'base_model'})
+
+@app.route("/")
+def index():
+   return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run()
